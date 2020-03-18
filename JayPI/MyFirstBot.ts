b880ae75d6ab86f7207ea5aaadf0726f17a4d29a
Jay Anwar <jayanwar@jay-anwars-mbp.home>
2020-03-18 12:28:33 +0000

    initial commit

---
+++ JayPI/MyFirstBot.ts
@@ -0,0 +1,27 @@
+import Discord from 'discord.js';
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.login(process.env.MYFIRSTBOT_TOKEN);
+
+client.on('ready', ()=>{
+    console.info(`Logged in as ${client.user.tag}`);
+})
+
+client.on('message', msg=>{
+    if (msg.content.startsWith('!img')) {
+        const searchQuery = msg.content.replace(/!img/,'').trim();
+        
+    } else if (msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+    } else if (msg.content.startsWith('!kick')) {
+        if (msg.mentions.users.size) {
+            const taggedUser = msg.mentions.users.first();
+            msg.channel.send(`You wanted a kick: ${taggedUser.username}`);
+        } else {
+            msg.reply('Tag a user next time...');
+        }
+    }
+})
