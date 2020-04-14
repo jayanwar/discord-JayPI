4dc332d996405ae1d50b1955f6971341c31ad530
Jay Anwar <j.anwar95@gmail.com>
2020-04-14 16:21:35 +0100

    Add list command for image handler

--- src/Command/handleImage/index.ts
+++ src/Command/handleImage/index.ts
@@ -3,8 +3,13 @@ import { CommandResult } from '../types';
 import axios from 'axios'
 import { readdirSync, createWriteStream } from 'fs';
 
-export default async function handleImageRequest(message: Discord.Message, args: string[], path: string) {
+export default async function handleImage(message: Discord.Message, args: string[], path: string) {
     const [name] = args;
+    const images = readdirSync(path);
+    if (name === 'list') {
+        await message.channel.send(images.join(','));
+        return;
+    }
     if (message.attachments && message.attachments.first()) {
         try {
             const response = await axios({
@@ -17,20 +22,25 @@ export default async function handleImageRequest(message: Discord.Message, args:
             const writer = createWriteStream(file);
             await response.data.pipe(writer);
             await message.channel.send(`Added image ${name}`);
+            return;
         } catch (e) {
             await message.channel.send(`Failed to add image ${name}`);
+            return;
         }
     } else {
         const filename = readdirSync(path).find(file => file.replace(/\.\w+$/,'') === name);
         try {
             if (filename) {
                 await message.channel.send({files: [`${path}/${filename}`]});
+                return;
             } else {
                 await message.channel.send(`Couldn't find image ${name}`);
+                return;
             }
         }
         catch (e) {
             await message.channel.send(`Something went wrong when trying to find image ${name}`);
+            return;
         }
     }
 }
